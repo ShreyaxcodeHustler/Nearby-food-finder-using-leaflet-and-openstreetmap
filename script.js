bc40832c7ad2423434d6f265f8dbc6c7e9459ef7
Shreya L <lshreya0505@gmail.com>
2026-06-13 21:26:34 +0530

    Added API Integration and dynamic place listing

--- script.js
+++ script.js
@@ -7,13 +7,39 @@ function getLocation(){
     navigator.geolocation.getCurrentPosition(showPosition, showError);
 }
 
-function showPosition(position){
+async function showPosition(position){
     const latitude=position.coords.latitude;
     const longitude=position.coords.longitude;
 
     output.textContent='Latitude:${latitude} Longitude:${longitude}';
+    searchNearby(latitude, longitude);
 }
 
 function showError(error){
     output.textContent="Location access denied or unavailable";
+}
+
+async function searchNearby(lat, lon){
+    const query =
+        `https://nominatim.openstreetmap.org/search?q=pav+bhaji&format=jsonv2&limit=10`;
+
+    try{
+        const response= await fetch(query);
+        const data =await response.json();
+        displayPlaces(data);
+
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+function displayPlaces(places){
+    const PlaceList= document.getElementById("placesList");
+    placesList.innerHTML="";
+    places.forEach(place=>{
+        const item= document.createElement("li");
+        item.textContent=place.display_name;
+        placesList.appendChild(item);
+    })
 }
